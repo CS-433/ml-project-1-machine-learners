b5a727a5acbf34c90fa25d62ec021be2d59047d3
vidyc <adrimart0155@gmail.com>
2024-11-01 11:49:51 +0100

    clean hyperparameter_selection script

--- hyperparameter_selection.py
+++ hyperparameter_selection.py
@@ -38,14 +38,21 @@ if __name__ == "__main__":
     # Set random seed to ensure that results are deterministic
     np.random.seed(42)
 
+    print("Reading processed datasets...")
     # Load already processed datasets
-    x_train = np.load("data/processed_x_train.npy")
-    x_test = np.load("data/processed_x_test.npy")
-    y_train = np.load("data/processed_y_train.npy")
+    x_train = np.load("data/processed_x_train.npz")["arr_0"]
+    x_test = np.load("data/processed_x_test.npz")["arr_0"]
+    y_train = np.load("data/processed_y_train.npz")["arr_0"]
+    print("Success!")
 
     # Test the sampling method :
+    print("Start Grid Search with the following potential params: ")
     prop = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
     learning_rates = [0.1, 0.15, 0.2]
     lambdas = [0.1, 0.05, 0.01, 0.005, 0]
+    print(f"Undersampling proportion: {prop}")
+    print(f"Learning rate: {learning_rates}")
+    print(f"Lambda: {lambdas}")
+    print("======")
     hyperparams_grid = list(itertools.product(prop, learning_rates, lambdas))
     compute_best_hyperparams(x_train, y_train, hyperparams_grid)
